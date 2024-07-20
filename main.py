adeda29a1a1bf2d58564360c3aa403f1db6f19ee
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-21 00:23:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 checks = []
 
+os.system("export LIGHTNING_USER_ID=c4a99537-57d8-4320-baa4-286d3f38b044")
+os.system("export LIGHTNING_API_KEY=10327400-e843-4a36-bbf0-6809d046826e")
 os.system("lightning login")
 
 
