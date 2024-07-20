a9d523813f4ebcab595e2ca9abafe58e75e9ad59
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-20 19:52:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def home():
     return f"Server is running,\nLate Update :{latest}"
 
 def run():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
 
 def keep_alive():
     t = Thread(target=run)
