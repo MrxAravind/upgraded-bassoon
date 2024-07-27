2576f2393c7a4e9712e89440d11e90118cfa7efe
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-07-27 17:25:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export LIGHTNING_USER_ID = 6516a2af-1430-4ca3-b6b8-afc5b4544c7d 
-export LIGHTNING_API_KEY = 05b1d656-150a-4d6a-842b-d6afa0c4ff41
+export LIGHTNING_USER_ID=6516a2af-1430-4ca3-b6b8-afc5b4544c7d 
+export LIGHTNING_API_KEY=05b1d656-150a-4d6a-842b-d6afa0c4ff41
 lightning login && python main.py
